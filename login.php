29cc8e45fab5ae0d4d6a2c040458d23fb7c796ef
Abdelrahman <abdoumohamed96@gmail.com>
2017-11-17 22:06:49 +0200

    make form fields required

--- login.php
+++ login.php
@@ -61,14 +61,14 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
      <div class="form-group">
       <label class="control-label col-sm-2" for="email">Email:</label>
       <div class="col-sm-10">
-       <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+       <input type="email" class="form-control" id="email" placeholder="Enter email" name="email" required>
     </div>
  </div>
 
  <div class="form-group">
    <label class="control-label col-sm-2" for="pwd">Password:</label>
    <div class="col-sm-10">          
-    <input type="password" class="form-control" id="password" placeholder="Enter password" name="password">
+    <input type="password" class="form-control" id="password" placeholder="Enter password" name="password" required>
  </div>
 </div>
 
--- register.php
+++ register.php
@@ -52,20 +52,20 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
   			<div class="form-group">
   				<label class="control-label col-sm-2" for="name">Name:</label>
   				<div class="col-sm-10">
-  					<input type="text" class="form-control" id="name" placeholder="Enter your name" name="name">
+  					<input type="text" class="form-control" id="name" placeholder="Enter your name" name="name" required>
   				</div>
   			</div>
   			<div class="form-group">
   				<label class="control-label col-sm-2" for="email">Email:</label>
   				<div class="col-sm-10">
-  					<input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+  					<input type="email" class="form-control" id="email" placeholder="Enter email" name="email" required>
   				</div>
   			</div>
 
   			<div class="form-group">
   				<label class="control-label col-sm-2" for="pwd">Password:</label>
   				<div class="col-sm-10">          
-  					<input type="password" class="form-control" id="password" placeholder="Enter password" name="password">
+  					<input type="password" class="form-control" id="password" placeholder="Enter password" name="password" required>
   				</div>
   			</div>
 
