27c6ac31e637ab473b30f6839d49619e9041ba97
Abdelrahman <abdoumohamed96@gmail.com>
2017-11-17 22:07:10 +0200

    SQL seed files for departments and courses

---
+++ course_seed.sql
@@ -0,0 +1,16 @@
+INSERT INTO courses (name, description, instructor_name, credit_hours, dept_id) VALUES
+("programming", "conditional, looping, file I/O, structs, all in C", "Saleh El-Shehaby", 3, 1),
+('database systems', 'data modelling, ER diagrams, SQL, query optimization, Indexes, and functional dependencies', "Yasser Fouad", 4, 1),
+('Operating Systems', 'Scheduling, Synchronization, Semaphores, Processes, threads, file systems, protection, memory management', 'Ahmed El-Sayed', 3, 1),
+("Algorithms", "Divide and Conquer, Dynamic Programming, Greedy Algorithms, Amortization", "Amr El-Marsy", 3, 1),
+("Compilers", "Lexical Analysis, Grammars, Finite Automata, Parsing, Code Generation, optimization, Garbage Collection", "Nagia Ghanem", 3, 1),
+("Thermo Dynamics", "Laws of Thermo Dynamics, ...", "Ma3rafsh", 3, 2),
+("Electric Circuits", "mesh Analysis, nodal Analysis, thevinen, kirchoff, AC", "Bassem Mokhtar", 3, 2),
+("Strength of Materials", "stress, strain, bla bla", "Someone", 3, 2),
+("Mechanics of Dynamics", "Impulse, momentum, energe, force, ...", "Abdelfattah Rizk", 3, 2),
+("Chemistry", "reactions, moles, more useless stuff", "Za3toot", 3, 3),
+("Fluid Dynamics", "how fluids flow", "Name here",3, 3),
+("Concrete", "Mixing cement with water","bardo ma3rafsh",3, 4),
+("Drawing", "Missing view, autocad", "maged naguib",3, 4),
+("Oil Rigs", "some useless stuff","Mobil 1",3, 5),
+("Fishing", "how to fish for fish","EL Sayyad",3, 5);
---
+++ departments_seed.sql
@@ -0,0 +1,6 @@
+INSERT INTO departments (name, description, id) VALUES
+("Computer and Communication Engineering", "student will learn how to program in several languages and Fundamentals of CS", 1),
+("Electro-Mechanical Engineering", "student will learn the fundamentals of electrical and mechanical engineering and their composite applications", 2),
+("Pertro-Chemical Engineering", "Chemistry and Pertoleum refinement", 3),
+("Construction and Archeticture Engineering", "How to build a skyscrapper", 4),
+("Off-Shore and Coastal Engineering", "How to fish for fish and get paid a lot", 5);
