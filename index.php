abdf24e7cac05dd6575e25d661b6cd6fabeeb1ea
Abdelrahman <abdoumohamed96@gmail.com>
2017-11-17 04:29:49 +0200

    make index point to sign in page

--- index.php
+++ index.php
@@ -1,13 +1,1 @@
-<html>
-	<body>
-
-		<form action="welcome.php" method="post">
-			Name: <input type="text" name="name"><br>
-			E-mail: <input type="text" name="email"><br>
-			Password: <input type="text" name="password"><br>
-
-			<input type="submit">
-		</form>
-
-	</body>
-</html>
+<<?php header("location:login.php") ?>
