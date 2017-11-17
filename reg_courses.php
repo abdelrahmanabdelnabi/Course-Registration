77f51f75bcd22c316f2c27f6732bc7ffc3d8d555
Abdelrahman <abdoumohamed96@gmail.com>
2017-11-17 02:49:04 +0200

    add basic styling to coure registration page

--- reg_courses.php
+++ reg_courses.php
@@ -1,39 +1,65 @@
-<?php
-require_once 'session.php';
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Course Registeration</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<div class="=container">
+		<div class="row">
+			<div class="col-lg-3">
+				<h1>Homepage</h1>
+				<h1>Courses</h1>
+			</div>
+			<div class="col-lg-9">
 
-	$dept_id = $_SESSION['dept_id'];
-	$sid = $_SESSION['user_id'];
+				<?php
+				require_once 'session.php';
 
-	// get the list of courses available in this department
-	$sql = "SELECT * FROM courses WHERE dept_id = ".$dept_id;
-	$result = mysqli_query($db,$sql);
+				$dept_id = $_SESSION['dept_id'];
+				$sid = $_SESSION['user_id'];
 
-	// get this student's enrollments
-	$enrol_query = "SELECT enrollments.*, courses.name FROM enrollments inner JOIN courses on enrollments.course_id=courses.id  WHERE sid=".$sid;
-	$enrols = mysqli_query($db, $enrol_query);
+		// get the list of courses available in this department
+				$sql = "SELECT * FROM courses WHERE dept_id = ".$dept_id;
+				$result = mysqli_query($db,$sql);
 
-	if(mysqli_num_rows($enrols) === 0) {
-		echo "<h2>You are currently not enrolled in any course</h2>";
-	} else {
-		echo "<h2>Your enrollments</h2>";
-		echo '<table>';
+		// get this student's enrollments
+				$enrol_query = "SELECT enrollments.*, courses.name FROM enrollments inner JOIN courses on enrollments.course_id=courses.id  WHERE sid=".$sid;
+				$enrols = mysqli_query($db, $enrol_query);
 
-		while($row = mysqli_fetch_array($enrols,MYSQLI_ASSOC)) {
-			$href ="enrol.php?course_id=".$row['course_id']."&sid=".$sid."&enrol=false";
-			echo '<tr><td>'.$row['name'].'</td><td><a href="'.$href.'">unenrol</a></td></tr>';
-		}
+				if(mysqli_num_rows($enrols) === 0) {
+					echo "<h2>You are currently not enrolled in any course</h2>";
+				} else {
+					echo "<h2>Your enrollments</h2>";
+					echo '<table class="table">';
+					echo '<tr><th>Course</th><th>un-register</th></tr>';
 
-		echo '</table>';		
-	}
+					while($row = mysqli_fetch_array($enrols,MYSQLI_ASSOC)) {
+						$href ="enrol.php?course_id=".$row['course_id']."&sid=".$sid."&enrol=false";
+						echo '<tr><td>'.$row['name'].'</td><td><a href="'.$href.'">unenrol</a></td></tr>';
+					}
 
-	echo '<table>';
-	echo '<tr>';
-	echo '<th>Course</th><th>register</th>';
-	echo '</tr>';
-	while($row = mysqli_fetch_array($result,MYSQLI_ASSOC)) {
-		echo '<tr><td>'.$row['name'].'</td><td><a href="enrol.php?course_id='.$row['id'].'&sid='.$sid.'&enrol=true'.'">enrol</a></td></tr>';
-	}
+					echo '</table>';		
+				}
 
-	echo '</table>';
+				echo '<h2>Available Courses</h2>';
+				echo '<table class="table">';
+				echo '<tr>';
+				echo '<th>Course</th><th>register</th>';
+				echo '</tr>';
+				while($row = mysqli_fetch_array($result,MYSQLI_ASSOC)) {
+					echo '<tr><td>'.$row['name'].'</td><td><a href="enrol.php?course_id='.$row['id'].'&sid='.$sid.'&enrol=true'.'">enrol</a></td></tr>';
+				}
 
- ?>
+				echo '</table>';
+
+				?>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
