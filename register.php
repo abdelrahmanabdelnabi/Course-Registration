e7b946b885795344c1b90c0915359eae755f92cd
Abdelrahman <abdoumohamed96@gmail.com>
2017-11-17 14:40:57 +0200

    add styling to sign-up page

--- register.php
+++ register.php
@@ -1,79 +1,91 @@
 <?php
 
-	require 'config.php';
+require 'config.php';
+$error = "";
 
-	if($_SERVER["REQUEST_METHOD"] == "POST") {
+if($_SERVER["REQUEST_METHOD"] == "POST") {
       // username and password sent from form 
-      $myname = mysqli_real_escape_string($db,$_POST['name']);
-      $myemail = mysqli_real_escape_string($db,$_POST['email']);
-      $mypassword = mysqli_real_escape_string($db,$_POST['password']);
-      
-      $sql = "SELECT id FROM users WHERE email = '$myemail'";
-      $result = mysqli_query($db,$sql);
-      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-      
+	$myname = mysqli_real_escape_string($db,$_POST['name']);
+	$myemail = mysqli_real_escape_string($db,$_POST['email']);
+	$mypassword = mysqli_real_escape_string($db,$_POST['password']);
+
+	$sql = "SELECT id FROM users WHERE email = '$myemail'";
+	$result = mysqli_query($db,$sql);
+	$row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+
       if($count == 1) { // email already in use
-         $_SESSION['message'] = "email already in use";
-         $error = "Your email is already in use";
-         header("location: register.php");
+      	$_SESSION['message'] = "email already in use";
+      	$error = "Your email is already in use";
+      	header("location: register.php");
       }else {
-         $insert = "INSERT INTO users (name, email, password) VALUES ('$myname','$myemail','$mypassword');";
+      	$insert = "INSERT INTO users (name, email, password) VALUES ('$myname','$myemail','$mypassword');";
 
-         if(mysqli_query($db, $insert)) {
-         	print_r("account created succesfully");
-         	$_SESSION['message'] = "account created successfully";
-         	header("location: welcome.php");
-         }
+      	if(mysqli_query($db, $insert)) {
+      		print_r("account created succesfully");
+      		$_SESSION['message'] = "account created successfully";
+      		header("location: welcome.php");
+      	}
       }
-   }
-?>
+  }
+  ?>
 
-<html>
-   
-   <head>
-      <title>Register Page</title>
-      
-      <style type = "text/css">
-         body {
-            font-family:Arial, Helvetica, sans-serif;
-            font-size:14px;
-         }
-         
-         label {
-            font-weight:bold;
-            width:100px;
-            font-size:14px;
-         }
-         
-         .box {
-            border:#666666 solid 1px;
-         }
-      </style>
-      
-   </head>
-   
-   <body bgcolor = "#FFFFFF">
-	
-      <div align = "center">
-         <div style = "width:300px; border: solid 1px #333333; " align = "left">
-            <div style = "background-color:#333333; color:#FFFFFF; padding:3px;"><b>Login</b></div>
-				
-            <div style = "margin:30px">
-               
-               <form action = "register.php" method = "post">
-               	<label>name  :</label><input type = "text" name = "name" class = "box"/><br /><br />
-                  <label>email  :</label><input type = "text" name = "email" class = "box"/><br /><br />
-                  <label>Password  :</label><input type = "password" name = "password" class = "box" /><br/><br />
-                  <input type = "submit" value = "Register"/><br />
-               </form>
-               
-               <div style = "font-size:11px; color:#cc0000; margin-top:10px"><?php if(isset($_SESSION['message']))echo $_SESSION['message']; ?></div>
-					
-            </div>
-				
-         </div>
-			
-      </div>
+  <html>
 
-   </body>
+  
+  <head>
+  	<title>Sign-up</title>
+  	<meta charset="utf-8">
+  	<meta name="viewport" content="width=device-width, initial-scale=1">
+  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+  </head>
+
+  <body>
+  	
+  	<div class="container">
+  		<h2>Sign-up</h2>
+
+  		<form class="form-horizontal" action="register.php" method="post">
+  			<div class="form-group">
+  				<label class="control-label col-sm-2" for="name">Name:</label>
+  				<div class="col-sm-10">
+  					<input type="text" class="form-control" id="name" placeholder="Enter your name" name="name">
+  				</div>
+  			</div>
+  			<div class="form-group">
+  				<label class="control-label col-sm-2" for="email">Email:</label>
+  				<div class="col-sm-10">
+  					<input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+  				</div>
+  			</div>
+
+  			<div class="form-group">
+  				<label class="control-label col-sm-2" for="pwd">Password:</label>
+  				<div class="col-sm-10">          
+  					<input type="password" class="form-control" id="password" placeholder="Enter password" name="password">
+  				</div>
+  			</div>
+
+  			<div class="form-group">        
+  				<div class="col-sm-offset-2 col-sm-10">
+  					<button type="submit" class="btn btn-default">Submit</button>
+  				</div>
+  			</div>
+
+  			<?php 
+  			if ($error !== "")
+  				echo '<div class="alert alert-danger">'.$error.'</div>';
+  			?>
+  		</form>
+  		<h5>Already have an account? sign-in <a href="login.php">here</a></h5>
+
+  	</div>
+
+  </div>
+
+</div>
+
+</body>
 </html>
